49876e6105dd362a6c143a1e875feda53fbe8382
sws2apps <sws2apps@gmail.com>
2021-12-25 00:15:46 +0300

    feat: initiall release

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+# [1.0.0-beta.3](https://github.com/sws2apps/sws2apps-api/compare/v1.0.0-beta.2...v1.0.0-beta.3) (2021-12-24)
+
+
+### Bug Fixes
+
+* **workflow:** update app name for deployment ([082a75e](https://github.com/sws2apps/sws2apps-api/commit/082a75eeb5ecd06ecd311e5b96a6fc45f1b79a88))
+
+# [1.0.0-beta.2](https://github.com/sws2apps/sws2apps-api/compare/v1.0.0-beta.1...v1.0.0-beta.2) (2021-12-24)
+
+
+### Bug Fixes
+
+* **workflow:** fix workflow typo error ([ac4cf55](https://github.com/sws2apps/sws2apps-api/commit/ac4cf55fa50eb105e8e0955cc312e27bb79a821d))
+
+# 1.0.0-beta.1 (2021-12-24)
+
+
+### Bug Fixes
+
+* **workflow:** update workflow settings ([d1df962](https://github.com/sws2apps/sws2apps-api/commit/d1df962947289e2cce1c314a08aab2fb6bf9461c))
+
+
+### Features
+
+* add semantic release ([b186222](https://github.com/sws2apps/sws2apps-api/commit/b186222931af4d5368a9c1e4c9ec7718203f4322))
---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const cors = require('cors');
+const limiter = require('express-rate-limit');
+
+var whitelist = ['https://sws-pocket.web.app', 'https://sws-pocket.firebaseapp.com', 'https://lmm-oa-sws.web.app', 'https://lmm-oa-sws.firebaseapp.com']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (process.env.NODE_ENV === 'production') {
+    const reqOrigin = req.header('Origin');
+    if (reqOrigin) {
+      if (whitelist.indexOf(reqOrigin) !== -1) {
+        corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+      } else {
+        // allow alpha & beta release
+        if (reqOrigin.startsWith("https://lmm-oa-sws--alpha") && reqOrigin.endsWith(".web.app")) {
+          corsOptions = { origin: true }
+        } else if (reqOrigin.startsWith("https://lmm-oa-sws--beta") && reqOrigin.endsWith(".web.app")) {
+          corsOptions = { origin: true }
+        } else if (reqOrigin.startsWith("https://sws-pocket--alpha") && reqOrigin.endsWith(".web.app")) {
+          corsOptions = { origin: true }
+        } else if (reqOrigin.startsWith("https://sws-pocket--beta") && reqOrigin.endsWith(".web.app")) {
+          corsOptions = { origin: true }
+        } else {
+          corsOptions = { origin: false } // disable CORS for this request
+        }
+      }
+    } else {
+      corsOptions = { origin: false }
+    }
+  } else {
+    corsOptions = { origin: true }; // allow cors during dev
+  }
+  
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+// Route import
+var appsRoute = require('./routes/apps');
+var swsPocketRoute = require('./routes/sws-pocket');
+
+const app = express();
+
+app.disable('x-powered-by');
+app.use(cors(corsOptionsDelegate));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(limiter({
+  windowMs: 20000,
+  max: 1,
+  message: JSON.stringify({
+    message: 'Too many requests, please try again later.'
+  })
+}))
+
+app.use('/api', appsRoute)
+app.use('/api/sws-pocket', swsPocketRoute)
+
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.send('SWS Apps API services')
+})
+
+// Handling invalid routes
+app.get('*', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.status(404).send(JSON.stringify({message: 'You are trying to access an invalid endpoint.'}));
+})
+
+app.post('*', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.status(404).send(JSON.stringify({message: 'You are trying to access an invalid endpoint.'}));
+})
+
+// Handling any other errors
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.send(JSON.stringify({message: error.message}))
+})
+
+app.listen(port, () => {
+  console.log(`server up and running`)
+})
