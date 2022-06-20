83a55fd17d0953ec52ff55bb3a7d65ab59275de8
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-06-21 00:06:37 +0300

    feat(users): add routes to manage user account profile from lmm-oa app

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,13 @@ import { visitorChecker } from '../middleware/visitor-checker.js';
 // utils import
 import { sendVerificationEmail } from '../utils/sendEmail.js';
 import { getAnnouncementsClient } from '../utils/announcement-utils.js';
-import { getUserInfo } from '../utils/user-utils.js';
+import {
+	findUserById,
+	getUserActiveSessions,
+	getUserInfo,
+	revokeSessions,
+} from '../utils/user-utils.js';
+import { decryptData } from '../utils/encryption-utils.js';
 
 // get firestore
 const db = getFirestore(); //get default database
@@ -22,7 +28,7 @@ router.post(
 	'/create-account',
 	body('fullname').isLength({ min: 3 }),
 	body('email').isEmail(),
-	body('password').isLength({ min: 6 }),
+	body('password').isLength({ min: 10 }),
 	async (req, res, next) => {
 		try {
 			const errors = validationResult(req);
@@ -107,12 +113,11 @@ router.use(visitorChecker());
 router.get('/validate-me', async (req, res, next) => {
 	try {
 		const { email } = req.headers;
-		const { cong_id, cong_name, cong_number, cong_role } = await getUserInfo(
-			email
-		);
+		const { id, cong_id, cong_name, cong_number, cong_role } =
+			await getUserInfo(email);
 
 		if (cong_name.length > 0) {
-			let obj = { cong_id, cong_name, cong_number, cong_role };
+			let obj = { id, cong_id, cong_name, cong_number, cong_role };
 
 			res.locals.type = 'info';
 			res.locals.message = 'visitor id has been validated';
@@ -153,5 +158,231 @@ router.get('/resend-verification', async (req, res, next) => {
 		next(err);
 	}
 });
+
+router.patch(
+	'/:id/fullname',
+	body('fullname').isLength({ min: 3 }),
+	async (req, res, next) => {
+		try {
+			const { id } = req.params;
+
+			if (id) {
+				const user = await findUserById(id);
+				if (user) {
+					const errors = validationResult(req);
+
+					if (!errors.isEmpty()) {
+						let msg = '';
+						errors.array().forEach((error) => {
+							msg += `${msg === '' ? '' : ', '}${error.param}: ${error.msg}`;
+						});
+
+						res.locals.type = 'warn';
+						res.locals.message = `invalid input: ${msg}`;
+
+						res.status(400).json({
+							message: 'Bad request: provided inputs are invalid.',
+						});
+
+						return;
+					}
+
+					const { fullname } = req.body;
+					await db
+						.collection('users')
+						.doc(id)
+						.update({ 'about.name': fullname });
+
+					res.locals.type = 'info';
+					res.locals.message = `the user fullname has been updated successfully`;
+					res.status(200).json({ fullname });
+				} else {
+					res.locals.type = 'warn';
+					res.locals.message = `the user could not be found`;
+					res.status(404).json({ message: 'USER_NOT_FOUND' });
+				}
+			} else {
+				res.locals.type = 'warn';
+				res.locals.message = `invalid input: user id is required`;
+				res.status(400).json({ message: 'USER_ID_INVALID' });
+			}
+		} catch (err) {
+			next(err);
+		}
+	}
+);
+
+router.patch(
+	'/:id/password',
+	body('password').isLength({ min: 10 }),
+	async (req, res, next) => {
+		try {
+			const { id } = req.params;
+
+			if (id) {
+				const user = await findUserById(id);
+				if (user) {
+					const errors = validationResult(req);
+
+					if (!errors.isEmpty()) {
+						let msg = '';
+						errors.array().forEach((error) => {
+							msg += `${msg === '' ? '' : ', '}${error.param}: ${error.msg}`;
+						});
+
+						res.locals.type = 'warn';
+						res.locals.message = `invalid input: ${msg}`;
+
+						res.status(400).json({
+							message: 'Bad request: provided inputs are invalid.',
+						});
+
+						return;
+					}
+
+					const { password } = req.body;
+					const { auth_uid } = user;
+
+					await getAuth().updateUser(auth_uid, { password: password });
+
+					res.locals.type = 'info';
+					res.locals.message = `the user password has been updated successfully`;
+					res.status(200).json({ message: 'OK' });
+				} else {
+					res.locals.type = 'warn';
+					res.locals.message = `the user could not be found`;
+					res.status(404).json({ message: 'USER_NOT_FOUND' });
+				}
+			} else {
+				res.locals.type = 'warn';
+				res.locals.message = `invalid input: user id is required`;
+				res.status(400).json({ message: 'USER_ID_INVALID' });
+			}
+		} catch (err) {
+			next(err);
+		}
+	}
+);
+
+router.get('/:id/2fa', async (req, res, next) => {
+	try {
+		const { id } = req.params;
+
+		if (id) {
+			const user = await findUserById(id);
+			if (user) {
+				// Retrieve user from database
+				const userRef = db.collection('users').doc(id);
+				const userSnap = await userRef.get();
+
+				// get encrypted token
+				const encryptedData = userSnap.data().about.secret;
+
+				// decrypt token
+				const decryptedData = decryptData(encryptedData);
+
+				// get uri and qr
+				const { secret, uri } = JSON.parse(decryptedData);
+
+				res.locals.type = 'info';
+				res.locals.message = `the user has fetched 2fa successfully`;
+				res.status(200).json({
+					secret: secret,
+					qrCode: uri,
+				});
+			} else {
+				res.locals.type = 'warn';
+				res.locals.message = `the user could not be found`;
+				res.status(404).json({ message: 'USER_NOT_FOUND' });
+			}
+		} else {
+			res.locals.type = 'warn';
+			res.locals.message = `invalid input: user id is required`;
+			res.status(400).json({ message: 'USER_ID_INVALID' });
+		}
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get('/:id/sessions', async (req, res, next) => {
+	try {
+		const { id } = req.params;
+
+		if (id) {
+			const user = await findUserById(id);
+			if (user) {
+				const sessions = await getUserActiveSessions(id);
+
+				res.locals.type = 'info';
+				res.locals.message = `the user has fetched sessions successfully`;
+				res.status(200).json(sessions);
+			} else {
+				res.locals.type = 'warn';
+				res.locals.message = `the user could not be found`;
+				res.status(404).json({ message: 'USER_NOT_FOUND' });
+			}
+		} else {
+			res.locals.type = 'warn';
+			res.locals.message = `invalid input: user id is required`;
+			res.status(400).json({ message: 'USER_ID_INVALID' });
+		}
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.delete(
+	'/:id/sessions',
+	body('session').notEmpty(),
+	async (req, res, next) => {
+		try {
+			const { id } = req.params;
+
+			if (id) {
+				const user = await findUserById(id);
+				if (user) {
+					const errors = validationResult(req);
+
+					if (!errors.isEmpty()) {
+						let msg = '';
+						errors.array().forEach((error) => {
+							msg += `${msg === '' ? '' : ', '}${error.param}: ${error.msg}`;
+						});
+
+						res.locals.type = 'warn';
+						res.locals.message = `invalid input: ${msg}`;
+
+						res.status(400).json({
+							message: 'Bad request: provided inputs are invalid.',
+						});
+
+						return;
+					}
+
+					const { session } = req.body;
+
+					await revokeSessions(id, session);
+
+					const sessions = await getUserActiveSessions(id);
+
+					res.locals.type = 'info';
+					res.locals.message = `the user has revoked session successfully`;
+					res.status(200).json(sessions);
+				} else {
+					res.locals.type = 'warn';
+					res.locals.message = `the user could not be found`;
+					res.status(404).json({ message: 'USER_NOT_FOUND' });
+				}
+			} else {
+				res.locals.type = 'warn';
+				res.locals.message = `invalid input: user and session id are required`;
+				res.status(400).json({ message: 'USER_ID_INVALID' });
+			}
+		} catch (err) {
+			next(err);
+		}
+	}
+);
 
 export default router;
