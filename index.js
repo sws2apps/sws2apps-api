2b74c8309471035cdeadd95e2abdb1e072a3bbe3
rhahao <rhahao.vj@gmail.com>
2022-09-13 18:00:38 +0300

    fix conflict in index.js

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ const APP_VERSION = process.env.npm_package_version;
 app.listen(PORT, () => {
 	logger(
 		'info',
-<<<<<<< HEAD
-		JSON.stringify({ message: `server up and running (v${APP_VERSION})` })
-=======
 		JSON.stringify({ details: `server up and running (v${APP_VERSION})` })
->>>>>>> 0742e65daeb1e4b43ef6c576621d70116fd342ed
 	);
 });
