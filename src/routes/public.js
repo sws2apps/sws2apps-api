e4167c8ab8c9e756f2ad8aa454bc597c24c3752a
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-09-17 22:29:36 +0300

    feat(public): move source material language from headers to params

--- src/routes/public.js
+++ src/routes/public.js
@@ -1,14 +1,13 @@
 import express from 'express';
-import { body } from 'express-validator';
-import { getUpdatedSchedules } from '../controllers/public-controller.js';
+import { check } from 'express-validator';
+import { getSchedules } from '../controllers/public-controller.js';
 
 const router = express.Router();
 
 // get updated and latest schedules
-router.post(
-	'/source-material',
-	body('language').notEmpty(),
-	getUpdatedSchedules
+router.get(
+	'/source-material/:language',
+	getSchedules
 );
 
 export default router;
