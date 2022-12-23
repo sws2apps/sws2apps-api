c1c17119d3bae2116b6997b761b1efa50a9cf2ab
rhahao <26148770+rhahao@users.noreply.github.com>
2022-12-23 20:02:12 +0300

    add project id as env

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -3,7 +3,7 @@ import { initializeApp, cert } from 'firebase-admin/app';
 const serviceAccount = JSON.parse(Buffer.from(process.env.GOOGLE_CONFIG_BASE64, 'base64').toString('ascii'));
 
 if (process.env.FIREBASE_AUTH_EMULATOR_HOST && process.env.FIRESTORE_EMULATOR_HOST) {
-  initializeApp({ projectId: 'dev-sws2apps' });
+  initializeApp({ projectId: process.env.FIREBASE_PROJECT_ID });
 } else {
   initializeApp({
     credential: cert(serviceAccount),
