52b0d8f52c0c2b564656b3da1b3b14fa99cc4f8a
rhahao <26148770+rhahao@users.noreply.github.com>
2023-01-08 20:12:15 +0300

    handle not found schedule

--- src/controllers/sws-pocket-controller.js
+++ src/controllers/sws-pocket-controller.js
@@ -97,8 +97,8 @@ export const getSchedule = async (req, res, next) => {
 		res.status(200).json({
 			cong_sourceMaterial,
 			cong_schedule,
-			class_count: cong_settings[0].class_count,
-			source_lang: cong_settings[0].source_lang,
+			class_count: cong_settings[0]?.class_count || 1,
+			source_lang: cong_settings[0]?.source_lang || 'e',
 		});
 	} catch (err) {
 		next(err);
