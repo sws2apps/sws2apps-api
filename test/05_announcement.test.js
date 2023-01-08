024c311c40031a65ef34ce82b444f51e0b8d3724
rhahao <26148770+rhahao@users.noreply.github.com>
2023-01-08 14:40:33 +0300

    update test files

--- test/05_announcement.test.js
+++ test/05_announcement.test.js
@@ -13,8 +13,23 @@ describe('Testing announcements', () => {
 			chai
 				.request(api)
 				.get('/api/users/announcement')
+				.send({ app: 'lmm-oa' })
 				.end((err, res) => {
-					expect(res.body).to.be.an('array');
+					expect(res).to.have.status(200);
+					expect(res.status).to.be.an('array');
+					done();
+				});
+		});
+	});
+
+	describe('GET /api/users/announcement', () => {
+		it('does not allow user to fetch announcements with invalid app', (done) => {
+			chai
+				.request(api)
+				.get('/api/users/announcement')
+				.send({ app: 'junk' })
+				.end((err, res) => {
+					expect(res).to.have.status(400);
 					done();
 				});
 		});
