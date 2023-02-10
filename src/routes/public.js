c81f0635a4d57c010114d4203d8ca4fae8ba4591
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2023-02-10 14:34:04 +0300

    feat(public): add routes to handle support for no epub languages

--- src/routes/public.js
+++ src/routes/public.js
@@ -1,9 +1,18 @@
-import express from "express";
-import { getSchedules } from "../controllers/public-controller.js";
+import express from 'express';
+import { getScheduleRawHTML, getSchedules, getSchedulesDocIds, isPublicationExist } from '../controllers/public-controller.js';
 
 const router = express.Router();
 
+// check if pub exist
+router.get('/source-material/:language/:issue', isPublicationExist);
+
+// get schedule doc ids
+router.get('/source-material/:language/:issue/docs-id', getSchedulesDocIds);
+
+// get schedule raw html text
+router.get('/source-material/:language/:docid/content', getScheduleRawHTML);
+
 // get updated and latest schedules
-router.get("/source-material/:language", getSchedules);
+router.get('/source-material/:language', getSchedules);
 
 export default router;
