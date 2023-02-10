1594b708e3d4d88de8957bfc8f807460491a43a7
rhahao <26148770+rhahao@users.noreply.github.com>
2023-02-10 14:28:00 +0300

    support for no epub language

--- src/controllers/public-controller.js
+++ src/controllers/public-controller.js
@@ -1,96 +1,9 @@
-import fetch from 'node-fetch';
-import { loadEPUB } from 'jw-epub-parser/dist/node/index.js';
-
-const fetchIssueData = (issue) => {
-	return new Promise((resolve) => {
-		if (issue.hasEPUB) {
-			const epubFile = issue.hasEPUB[0].file;
-			const epubUrl = epubFile.url;
-			const epubModifiedDate = epubFile.modifiedDatetime;
-
-			loadEPUB({ url: epubUrl }).then((epubData) => {
-				const obj = {
-					issueDate: issue.issueDate,
-					modifiedDateTime: epubModifiedDate,
-					...epubData,
-				};
-				resolve(obj);
-			});
-		}
-	});
-};
-
-const fetchData = async (language) => {
-	const mergedSources = [];
-	let notFound = false;
-
-	// get current issue
-	const today = new Date();
-	const day = today.getDay();
-	const diff = today.getDate() - day + (day === 0 ? -6 : 1);
-	const weekDate = new Date(today.setDate(diff));
-	const currentMonth = weekDate.getMonth() + 1;
-	const monthOdd = currentMonth % 2 === 0 ? false : true;
-	let monthMwb = monthOdd ? currentMonth : currentMonth - 1;
-	let currentYear = weekDate.getFullYear();
-
-	const issues = [];
-
-	do {
-		const issueDate = currentYear + String(monthMwb).padStart(2, '0');
-		const url =
-			process.env.JW_CDN +
-			new URLSearchParams({
-				langwritten: language,
-				pub: 'mwb',
-				fileformat: 'epub',
-				output: 'json',
-				issue: issueDate,
-			});
-
-		const res = await fetch(url);
-
-		if (res.status === 200) {
-			const result = await res.json();
-			const hasEPUB = result.files[language].EPUB;
-
-			issues.push({ issueDate, currentYear, language, hasEPUB: hasEPUB });
-		}
-
-		if (res.status === 404) {
-			notFound = true;
-		}
-
-		// assigning next issue
-		monthMwb = monthMwb + 2;
-		if (monthMwb === 13) {
-			monthMwb = 1;
-			currentYear++;
-		}
-	} while (notFound === false);
-
-	if (issues.length > 0) {
-		const fetchSource1 = fetchIssueData(issues[0]);
-		const fetchSource2 = issues.length > 1 ? fetchIssueData(issues[1]) : Promise.resolve({});
-		const fetchSource3 = issues.length > 2 ? fetchIssueData(issues[2]) : Promise.resolve({});
-		const fetchSource4 = issues.length > 3 ? fetchIssueData(issues[3]) : Promise.resolve({});
-
-		const allData = await Promise.all([fetchSource1, fetchSource2, fetchSource3, fetchSource4]);
-
-		for (let z = 0; z < allData.length; z++) {
-			const tempObj = allData[z];
-			if (tempObj.issueDate) {
-				mergedSources.push(tempObj);
-			}
-		}
-	}
-
-	return mergedSources;
-};
+import { extractScheduleDocsId, fetchData } from '../utils/public-utils.js';
 
 export const getSchedules = async (req, res, next) => {
 	try {
 		let { language } = req.params;
+
 		language = language.toUpperCase();
 
 		const mergedSources = await fetchData(language);
@@ -104,6 +17,103 @@ export const getSchedules = async (req, res, next) => {
 			res.locals.message = 'schedules could not be fetched because language is invalid or not available yet';
 			res.status(404).json({ message: 'FETCHING_FAILED' });
 		}
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const isPublicationExist = async (req, res, next) => {
+	try {
+		let { language, issue } = req.params;
+		language = language.toUpperCase();
+
+		const url =
+			process.env.JW_CDN +
+			new URLSearchParams({
+				langwritten: language,
+				pub: 'mwb',
+				output: 'json',
+				issue,
+			});
+
+		const resLocal = await fetch(url);
+
+		if (resLocal.status === 200) {
+			const result = await resLocal.json();
+
+			res.locals.type = 'info';
+			res.locals.message = 'publication check successfull';
+			res.status(200).json(result);
+			return;
+		}
+
+		res.locals.type = 'warn';
+		res.locals.message = 'publication could not be found';
+		res.status(404).json({ message: 'PUBLICATION_NOT_FOUND' });
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const getSchedulesDocIds = async (req, res, next) => {
+	try {
+		let { language, issue } = req.params;
+		language = language.toUpperCase();
+
+		const url =
+			process.env.JW_FINDER +
+			new URLSearchParams({
+				wtlocale: language,
+				pub: 'mwb',
+				issue,
+			});
+
+		const resLocal = await fetch(url);
+
+		if (resLocal.status === 200) {
+			const result = await resLocal.text();
+			const docsId = await extractScheduleDocsId(result);
+
+			res.locals.type = 'info';
+			res.locals.message = 'publication schedules document id extracted';
+			res.status(200).json(docsId);
+			return;
+		}
+
+		res.locals.type = 'warn';
+		res.locals.message = 'error getting document id: publication could not be found';
+		res.status(404).json({ message: 'PUBLICATION_NOT_FOUND' });
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const getScheduleRawHTML = async (req, res, next) => {
+	try {
+		let { language, docid } = req.params;
+		language = language.toUpperCase();
+
+		const url =
+			process.env.JW_FINDER +
+			new URLSearchParams({
+				wtlocale: language,
+				docid,
+			});
+
+		const resLocal = await fetch(url);
+
+		if (resLocal.status === 200) {
+			const result = await resLocal.text();
+
+			res.locals.type = 'info';
+			res.locals.message = 'publication content successfully extracted';
+			res.status(200).json(result);
+			return;
+		}
+
+		res.locals.type = 'warn';
+		res.locals.message = 'publication could not be found';
+		res.status(404).json({ message: 'PUBLICATION_NOT_FOUND' });
 	} catch (err) {
 		next(err);
 	}
---
+++ src/utils/public-utils.js
@@ -0,0 +1,107 @@
+import 'global-jsdom/register';
+import fetch from 'node-fetch';
+import { loadEPUB } from 'jw-epub-parser/dist/node/index.js';
+
+export const fetchIssueData = (issue) => {
+	return new Promise((resolve) => {
+		if (issue.hasEPUB) {
+			const epubFile = issue.hasEPUB[0].file;
+			const epubUrl = epubFile.url;
+			const epubModifiedDate = epubFile.modifiedDatetime;
+
+			loadEPUB({ url: epubUrl }).then((epubData) => {
+				const obj = {
+					issueDate: issue.issueDate,
+					modifiedDateTime: epubModifiedDate,
+					...epubData,
+				};
+				resolve(obj);
+			});
+		}
+	});
+};
+
+export const fetchData = async (language) => {
+	const mergedSources = [];
+	let notFound = false;
+
+	// get current issue
+	const today = new Date();
+	const day = today.getDay();
+	const diff = today.getDate() - day + (day === 0 ? -6 : 1);
+	const weekDate = new Date(today.setDate(diff));
+	const currentMonth = weekDate.getMonth() + 1;
+	const monthOdd = currentMonth % 2 === 0 ? false : true;
+	let monthMwb = monthOdd ? currentMonth : currentMonth - 1;
+	let currentYear = weekDate.getFullYear();
+
+	const issues = [];
+
+	do {
+		const issueDate = currentYear + String(monthMwb).padStart(2, '0');
+		const url =
+			process.env.JW_CDN +
+			new URLSearchParams({
+				langwritten: language,
+				pub: 'mwb',
+				fileformat: 'epub',
+				output: 'json',
+				issue: issueDate,
+			});
+
+		const res = await fetch(url);
+
+		if (res.status === 200) {
+			const result = await res.json();
+			const hasEPUB = result.files[language].EPUB;
+
+			issues.push({ issueDate, currentYear, language, hasEPUB: hasEPUB });
+		}
+
+		if (res.status === 404) {
+			notFound = true;
+		}
+
+		// assigning next issue
+		monthMwb = monthMwb + 2;
+		if (monthMwb === 13) {
+			monthMwb = 1;
+			currentYear++;
+		}
+	} while (notFound === false);
+
+	if (issues.length > 0) {
+		const fetchSource1 = fetchIssueData(issues[0]);
+		const fetchSource2 = issues.length > 1 ? fetchIssueData(issues[1]) : Promise.resolve({});
+		const fetchSource3 = issues.length > 2 ? fetchIssueData(issues[2]) : Promise.resolve({});
+		const fetchSource4 = issues.length > 3 ? fetchIssueData(issues[3]) : Promise.resolve({});
+
+		const allData = await Promise.all([fetchSource1, fetchSource2, fetchSource3, fetchSource4]);
+
+		for (let z = 0; z < allData.length; z++) {
+			const tempObj = allData[z];
+			if (tempObj.issueDate) {
+				mergedSources.push(tempObj);
+			}
+		}
+	}
+
+	return mergedSources;
+};
+
+export const extractScheduleDocsId = async (htmlText) => {
+	const parser = new window.DOMParser();
+	const htmlItem = parser.parseFromString(htmlText, 'text/html');
+
+	const docIds = [];
+	const accordionItems = htmlItem.getElementsByClassName('docClass-106');
+	for (const weekLink of accordionItems) {
+		weekLink.classList.forEach((item) => {
+			if (item.indexOf('docId-') !== -1) {
+				docIds.push(item.split('-')[1]);
+			}
+		});
+	}
+
+	return docIds;
+};
