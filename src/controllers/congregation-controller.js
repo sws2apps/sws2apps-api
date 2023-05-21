ddbf570ee85e9ec67e3a7c9f2007c5e830dbce4b
rhahao <26148770+rhahao@users.noreply.github.com>
2023-05-21 09:49:57 +0300

    feat(controllers): include opening prayer autoassign setting when fetching schedules

--- src/controllers/congregation-controller.js
+++ src/controllers/congregation-controller.js
@@ -1196,10 +1196,13 @@ export const getMeetingSchedules = async (req, res, next) => {
 					res.status(200).json({
 						cong_sourceMaterial,
 						cong_schedule,
-						class_count: cong_settings[0]?.class_count,
-						source_lang: cong_settings[0]?.source_lang,
-						co_name: cong_settings[0]?.co_name || '',
-						co_displayName: cong_settings[0]?.co_displayName || '',
+						cong_settings: {
+							class_count: cong_settings[0]?.class_count || 1,
+							source_lang: cong_settings[0]?.source_lang || 'e',
+							co_name: cong_settings[0]?.co_name || '',
+							co_displayName: cong_settings[0]?.co_displayName || '',
+							opening_prayer_autoAssign: cong_settings[0]?.opening_prayer_autoAssign || false,
+						},
 					});
 					return;
 				}
