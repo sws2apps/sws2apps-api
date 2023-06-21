d7814f8b3873514a61383746f623a51d4bb51fa2
rhahao <26148770+rhahao@users.noreply.github.com>
2023-06-21 19:19:53 +0300

    fix(routes): move approving and disapproving field service report routes

--- src/routes/congregation.js
+++ src/routes/congregation.js
@@ -1,32 +1,17 @@
 import express from 'express';
 import { body, check, oneOf } from 'express-validator';
-import { congregationAdminChecker } from '../middleware/congregation-admin-checker.js';
 import { congregationRoleChecker } from '../middleware/congregation-role-checker.js';
 import { visitorChecker } from '../middleware/visitor-checker.js';
 import {
-	addCongregationUser,
 	createCongregation,
-	createNewPocketUser,
-	deletePocketDevice,
-	deletePocketOTPCode,
-	findUserByCongregation,
-	generatePocketOTPCode,
 	getCongregationBackup,
-	getCongregationMembers,
-	getCongregationPockerUser,
 	getCongregations,
-	getCongregationUser,
 	getCountries,
 	getLastCongregationBackup,
 	getMeetingSchedules,
-	removeCongregationUser,
 	saveCongregationBackup,
 	sendPocketSchedule,
 	updateCongregationInfo,
-	updateCongregationMemberDetails,
-	updatePocketDetails,
-	updateMembersDelegate,
-	updatePocketUsername,
 	postUserFieldServiceReports,
 	unpostUserFieldServiceReports,
 	getPendingFieldServiceReports,
@@ -100,67 +85,6 @@ router.patch(
 
 // post new sws pocket schedule
 router.post('/:id/schedule', body('schedules').isArray(), body('cong_settings').isArray(), sendPocketSchedule);
-
-// activate congregation admin role checker
-router.use(congregationAdminChecker());
-
-// get congregation users with roles
-router.get('/:id/members', getCongregationMembers);
-
-// find user by email from congregation
-router.get('/:id/members/find?:search', findUserByCongregation);
-
-// get congregation user
-router.get('/:id/members/:user', getCongregationUser);
-
-// remove user from congregation
-router.delete('/:id/members/:user', removeCongregationUser);
-
-// add user to congregation
-router.put('/:id/members', body('user_id').isString(), addCongregationUser);
-
-// update user role in congregation
-router.patch(
-	'/:id/members/:user',
-	body('user_role').isArray(),
-	body('user_members_delegate').isArray(),
-	updateCongregationMemberDetails
-);
-
-// get congregation pocket user
-router.get('/:id/pockets/:user', getCongregationPockerUser);
-
-// create new pocket user
-router.put(
-	'/:id/pockets',
-	body('user_local_uid').notEmpty().isString(),
-	body('username').notEmpty().isString(),
-	createNewPocketUser
-);
-
-// update pocket member
-router.patch(
-	'/:id/pockets/:user',
-	body('user_local_uid').notEmpty().isString(),
-	body('user_role').isArray(),
-	body('user_members_delegate').isArray(),
-	updatePocketDetails
-);
-
-// update pocket username
-router.patch('/:id/pockets/:user/username', body('username').notEmpty(), updatePocketUsername);
-
-// update pocket members
-router.patch('/:id/pockets/:user/members', body('members').notEmpty(), updateMembersDelegate);
-
-// generate new pocket otp code
-router.get('/:id/pockets/:user/code', generatePocketOTPCode);
-
-// delete pocket otp code
-router.delete('/:id/pockets/:user/code', deletePocketOTPCode);
-
-// delete pocket device
-router.delete('/:id/pockets/:user', body('pocket_visitorid').notEmpty(), deletePocketDevice);
 
 // get pending field service reports
 router.get('/:id/field-service-reports', getPendingFieldServiceReports);
