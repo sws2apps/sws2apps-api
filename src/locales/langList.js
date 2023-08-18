7d92c81f53611df3ba5c29caa79687797df803a0
rhahao <26148770+rhahao@users.noreply.github.com>
2023-08-18 17:51:50 +0300

    feat(locales): support German language

--- src/locales/langList.js
+++ src/locales/langList.js
@@ -1,5 +1,6 @@
 export const LANGUAGE_LIST = [
 	{ code: 'e', locale: 'en', name: 'English' },
+	{ code: 'x', locale: 'de-DE', name: 'German' },
 	{ code: 'mg', locale: 'mg-MG', name: 'Malagasy' },
 	{ code: 't', locale: 'pt-BR', name: 'Português (Brasil)' },
 	{ code: 'k', locale: 'uk-UA', name: 'українська' },
