24f848b52bc3eb7eacbb8ffc66225cbafb26952e
rhahao <26148770+rhahao@users.noreply.github.com>
2023-09-16 23:13:49 +0300

    build(api): add .gitkeep to push empty folders

--- src/dev/setup.js
+++ src/dev/setup.js
@@ -10,31 +10,33 @@ export const importPublicTalks = async () => {
 	const files = await fs.readdir('./src/dev/S-34');
 
 	for await (const file of files) {
-		const filepath = rootDir + '/' + file;
-		const content = await fs.readFile(filepath, 'utf-8');
-		const talks = JSON.parse(content);
-		const language = file.split('.')[0].toUpperCase();
+		if (file.endsWith('.json')) {
+			const filepath = rootDir + '/' + file;
+			const content = await fs.readFile(filepath, 'utf-8');
+			const talks = JSON.parse(content);
+			const language = file.split('.')[0].toUpperCase();
 
-		const talksToUpdate = [];
+			const talksToUpdate = [];
 
-		for (const talk of talks) {
-			const findTalk = publicTalks.find(talk.talk_number);
-			let update = false;
+			for (const talk of talks) {
+				const findTalk = publicTalks.find(talk.talk_number);
+				let update = false;
 
-			if (!findTalk) update = true;
+				if (!findTalk) update = true;
 
-			if (findTalk && !findTalk[language]) update = true;
+				if (findTalk && !findTalk[language]) update = true;
 
-			if (findTalk && findTalk[language] && talk.talk_title !== findTalk[language].title) update = true;
+				if (findTalk && findTalk[language] && talk.talk_title !== findTalk[language].title) update = true;
 
-			if (update) {
-				talksToUpdate.push(talk);
+				if (update) {
+					talksToUpdate.push(talk);
+				}
 			}
-		}
 
-		for await (const talk of talksToUpdate) {
-			const payload = { talk_number: talk.talk_number, title: talk.title, modified: new Date().toISOString() };
-			await publicTalks.update(language, payload);
+			for await (const talk of talksToUpdate) {
+				const payload = { talk_number: talk.talk_number, title: talk.title, modified: new Date().toISOString() };
+				await publicTalks.update(language, payload);
+			}
 		}
 	}
 };
