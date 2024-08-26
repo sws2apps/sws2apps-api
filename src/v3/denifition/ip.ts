5ff8b269221151cca6bbc229329a585228a2b6a8
rhahao <26148770+rhahao@users.noreply.github.com>
2024-08-26 16:42:53 +0300

    feat(services): support multiple services for ip details

---
+++ src/v3/denifition/ip.ts
@@ -0,0 +1,8 @@
+export type IPAPIResponse = {
+	continent_code: string;
+	country_name: string;
+	country_code_iso3: string;
+	country_code: string;
+	city: string;
+	timezone: string | string[];
+};
--- src/v3/services/ip_details/auth_utils.ts
+++ src/v3/services/ip_details/auth_utils.ts
@@ -1,24 +1,69 @@
 import { Request } from 'express';
 import fetch from 'node-fetch';
 import WhichBrowser from 'which-browser';
+import { IPAPIResponse } from '../../denifition/ip.js';
 
-type IPAPIResponse = {
-	continentCode: string | undefined;
-	countryName: string | undefined;
-	countryCode: string | undefined;
-	cityName: string | undefined;
-	timeZones: string[] | undefined;
-};
+const APIs = [
+	{
+		host: 'https://ipapi.co/${visitorIP}/json',
+		map: {
+			continent_code: 'continent_code',
+			country_name: 'country_name',
+			country_code: 'country_code',
+			country_code_iso3: 'country_code_iso3',
+			city: 'city',
+			timezone: 'timezone',
+		},
+	},
+	{
+		host: 'https://freeipapi.com/api/json/${visitorIP}',
+		map: {
+			continentCode: 'continent_code',
+			countryName: 'country_name',
+			countryCode: 'country_code',
+			cityName: 'city',
+			timeZones: 'timezone',
+		},
+	},
+];
 
 export const retrieveVisitorDetails = async (visitorIP: string, req: Request) => {
 	try {
-		const resIP = await fetch(`https://freeipapi.com/api/json/${visitorIP}`);
+		const resultAPI = {} as Record<string, string | string[]>;
 
-		if (resIP.status !== 200) {
-			throw new Error('THIRDY_PARTY_ERROR');
+		let currentIndex = 0;
+		for await (const api of APIs) {
+			try {
+				const host = api.host.replace('${visitorIP}', visitorIP);
+
+				const res = await fetch(host);
+
+				if (res.status !== 200 && currentIndex === APIs.length - 1) {
+					throw new Error('THIRDY_PARTY_ERROR_IP_DETAILS');
+				}
+
+				if (res.status === 200) {
+					const dataIP = (await res.json()) as Record<string, string | string[]>;
+
+					for (const [key, value] of Object.entries(api.map)) {
+						resultAPI[value] = dataIP[key];
+					}
+
+					break;
+				}
+				currentIndex++;
+			} catch {
+				if (currentIndex === APIs.length - 1) {
+					throw new Error('THIRDY_PARTY_ERROR_IP_DETAILS');
+				}
+
+				currentIndex++;
+
+				continue;
+			}
 		}
 
-		const dataIP = (await resIP.json()) as IPAPIResponse;
+		const dataIP = resultAPI as IPAPIResponse;
 
 		const result = new WhichBrowser(req.headers);
 
@@ -27,11 +72,11 @@ export const retrieveVisitorDetails = async (visitorIP: string, req: Request) =>
 			os: result.os.toString(),
 			ip: visitorIP,
 			ipLocation: {
-				continent_code: dataIP.continentCode || '',
-				country_name: dataIP.countryName || '',
-				country_code: dataIP.countryCode || '',
-				city: dataIP.cityName || '',
-				timezone: dataIP.timeZones || '',
+				continent_code: dataIP.continent_code || '',
+				country_name: dataIP.country_name || '',
+				country_code: dataIP.country_code_iso3 || dataIP.country_code || '',
+				city: dataIP.city || '',
+				timezone: dataIP.timezone || '',
 			},
 			isMobile: result.getType() !== 'desktop',
 		};
