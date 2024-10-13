3cafcfcc86f1e10fbaa6a76a3e9cdc7f065d1093
rhahao <26148770+rhahao@users.noreply.github.com>
2024-10-13 19:26:43 +0300

    update condition to enable logtail

--- src/v3/services/logger/logger.ts
+++ src/v3/services/logger/logger.ts
@@ -1,24 +1,20 @@
 import { Logtail } from '@logtail/node';
 
-const isProd = process.env.NODE_ENV === 'production';
-const isDev = process.env.NODE_ENV === 'development';
-const sourceToken = process.env.LOGTAIL_SOURCE_TOKEN!;
+const sourceToken = process.env.LOGTAIL_SOURCE_TOKEN;
 
 export const logger = (level: string, message: string) => {
-	if (isProd || isDev) {
-		const logtail = isProd ? new Logtail(sourceToken) : undefined;
+	const logtail = sourceToken ? new Logtail(sourceToken) : undefined;
 
-		if (level === 'info') {
-			console.log(message);
-			if (logtail) logtail.info(message);
-		} else if (level === 'warn') {
-			console.warn(message);
-			if (logtail) logtail.warn(message);
-		} else if (level === 'error') {
-			console.error(message);
-			if (logtail) logtail.error(message);
-		}
-
-		if (logtail) logtail.flush();
+	if (level === 'info') {
+		console.log(message);
+		if (logtail) logtail.info(message);
+	} else if (level === 'warn') {
+		console.warn(message);
+		if (logtail) logtail.warn(message);
+	} else if (level === 'error') {
+		console.error(message);
+		if (logtail) logtail.error(message);
 	}
+
+	if (logtail) logtail.flush();
 };
