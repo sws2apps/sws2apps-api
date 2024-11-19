a14f47bb3f395504b3cf571e7ab2977d587da5ca
rhahao <26148770+rhahao@users.noreply.github.com>
2024-11-19 21:39:08 +0300

    chore(firebase): set ability to set storage endpoint

--- src/v3/config/firebase_config.ts
+++ src/v3/config/firebase_config.ts
@@ -2,8 +2,10 @@ import { initializeApp, cert } from 'firebase-admin/app';
 
 const serviceAccount = JSON.parse(Buffer.from(process.env.GOOGLE_CONFIG_BASE64!, 'base64').toString('ascii'));
 
+const suffix = process.env.FIREBASE_STORAGE_SUFFIX || 'firebasestorage.app';
+
 initializeApp({
 	projectId: process.env.FIREBASE_APP_NAME,
 	credential: cert(serviceAccount),
-	storageBucket: `${process.env.FIREBASE_APP_NAME}.firebasestorage.app`,
+	storageBucket: `${process.env.FIREBASE_APP_NAME}.${suffix}`,
 });
