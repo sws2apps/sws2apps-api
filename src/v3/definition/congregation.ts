ddf8a8f798cc7d75b4527821761df34576e59c16
Max Makaluk <m.makaliuk@gmail.com>
2025-01-28 22:49:11 +0200

    fix(definition): change key name and type in CongSettingsType

--- src/v3/definition/congregation.ts
+++ src/v3/definition/congregation.ts
@@ -157,7 +157,7 @@ export type CongSettingsType = {
 	attendance_online_record?: string;
 	responsabilities?: string;
 	last_backup: string;
-	field_service_group_publishers_sort_method?: {value: number;  updatedAt: string};
+	group_publishers_sort?: string;
 };
 
 export type BackupData = {
