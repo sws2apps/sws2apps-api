2f1d03e1fdd260e47a21d1ca7cda81f15e461ea3
rhahao <26148770+rhahao@users.noreply.github.com>
2025-01-30 16:27:30 +0300

    fix(services): update users response for admin

--- src/v3/services/api/users.ts
+++ src/v3/services/api/users.ts
@@ -1,9 +1,13 @@
+import { CongregationsList } from '../../classes/Congregations.js';
 import { UsersList } from '../../classes/Users.js';
 
 export const adminUsersGet = async () => {
 	const users = UsersList.list;
 
 	const result = users.map((user) => {
+		const congId = user.profile.congregation?.id || '';
+		const cong = CongregationsList.findById(congId);
+
 		return {
 			id: user.id,
 			sessions: user.sessions,
@@ -13,6 +17,12 @@ export const adminUsersGet = async () => {
 				mfa_enabled: user.profile.mfa_enabled,
 				global_role: user.profile.role,
 				role: undefined,
+				congregation: {
+					...user.profile.congregation,
+					country_code: cong?.settings.country_code || '',
+					cong_name: cong?.settings.cong_name || '',
+					cong_number: cong?.settings.cong_number || '',
+				},
 			},
 		};
 	});
