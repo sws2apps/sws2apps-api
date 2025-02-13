5c99ac37da155369bf8e577ea017978677d3a436
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2025-02-13 12:42:41 +0300

    chore(locales): remove outdated folders

--- src/v3/constant/langList.ts
+++ src/v3/constant/langList.ts
@@ -3,7 +3,7 @@ export const ALL_LANGUAGES = [
 	{ locale: 'en', name: 'English', code: 'E' },
 	{ locale: 'mg-MG', name: 'Malagasy', code: 'MG' },
 	{ locale: 'pl-PL', name: 'Polski', code: 'P' },
-	{ locale: 'pt-BR', name: 'Português (Brasil)', code: 'T' },
+	{ locale: 'pt-POR', name: 'Português (Brasil)', code: 'T' },
 	{ locale: 'ru-RU', name: 'русский', code: 'U' },
 	{ locale: 'mg-TTM', name: 'Tenin’ny Tanana Malagasy', code: 'TTM' },
 	{ locale: 'uk-UA', name: 'Українська', code: 'K' },
