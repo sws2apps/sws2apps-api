64f063b77ee6bc16440616cff8fc964e68247f34
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2025-05-20 08:53:58 +0300

    chore(controllers): fix invalid import

--- src/v3/controllers/congregation_controller.ts
+++ src/v3/controllers/congregation_controller.ts
@@ -6,7 +6,7 @@ import { ApiCongregationSearchResponse } from '../definition/congregation.js';
 import { formatError } from '../utils/format_log.js';
 import { StandardRecord } from '../definition/app.js';
 import { MailClient } from '../config/mail_config.js';
-import { ALL_LANGUAGES } from '../constant/langList';
+import { ALL_LANGUAGES } from '../constant/langList.js';
 
 const MAIL_ENABLED = process.env.MAIL_ENABLED === 'true';
 
