ff1a9398d02d0c2fd8565e367d9428fc07484cc6
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2025-05-20 08:23:09 +0300

    fix(constant): update locale key from email localization

--- src/v3/constant/langList.ts
+++ src/v3/constant/langList.ts
@@ -1,10 +1,10 @@
 export const ALL_LANGUAGES = [
-	{ locale: 'de-DE', name: 'Deutsch', code: 'X' },
-	{ locale: 'en', name: 'English', code: 'E' },
-	{ locale: 'mg-MG', name: 'Malagasy', code: 'MG' },
-	{ locale: 'pl-PL', name: 'Polski', code: 'P' },
-	{ locale: 'pt-POR', name: 'Português (Brasil)', code: 'T' },
-	{ locale: 'ru-RU', name: 'русский', code: 'U' },
-	{ locale: 'mg-TTM', name: 'Tenin’ny Tanana Malagasy', code: 'TTM' },
-	{ locale: 'uk-UA', name: 'Українська', code: 'K' },
+	{ locale: 'deu', name: 'Deutsch', code: 'X' },
+	{ locale: 'eng', name: 'English', code: 'E' },
+	{ locale: 'mlg', name: 'Malagasy', code: 'MG' },
+	{ locale: 'pol', name: 'Polski', code: 'P' },
+	{ locale: 'por', name: 'Português (Brasil)', code: 'T' },
+	{ locale: 'rus', name: 'русский', code: 'U' },
+	{ locale: 'ttm', name: 'Tenin’ny Tanana Malagasy', code: 'TTM' },
+	{ locale: 'ukr', name: 'Українська', code: 'K' },
 ];
