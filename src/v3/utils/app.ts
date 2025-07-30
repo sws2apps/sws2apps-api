cff3d4303b7cdc62c851fc1eb7a346e81cd2f1ed
rhahao <26148770+rhahao@users.noreply.github.com>
2025-07-30 19:00:18 +0300

    chore(review): apply coderabbit fixes

--- src/v3/utils/app.ts
+++ src/v3/utils/app.ts
@@ -1,4 +1,5 @@
 import { CookieOptions, Request } from 'express';
+import { API_VAR } from '../../index.js';
 
 const isLocalRequest = (req: Request) => {
 	const host = req.get('host');
@@ -15,4 +16,20 @@ export const cookieOptions = (req: Request): CookieOptions => {
 		sameSite: isLocalHost ? 'lax' : 'none',
 		maxAge: 400 * 24 * 60 * 60 * 1000,
 	};
+};
+
+export const isValidClientVersion = (version: string) => {
+	const parts1 = version.split('.').map(Number);
+	const parts2 = API_VAR.MINIMUM_APP_VERSION.split('.').map(Number);
+
+	const maxLength = Math.max(parts1.length, parts2.length);
+	for (let i = 0; i < maxLength; i++) {
+		const num1 = parts1[i] ?? 0;
+		const num2 = parts2[i] ?? 0;
+
+		if (num1 > num2) return true;
+		if (num1 < num2) return false;
+	}
+
+	return true;
 };
