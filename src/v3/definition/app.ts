d59d6a1768d6a34b34ec5b25401d79bc8114e876
rhahao <26148770+rhahao@users.noreply.github.com>
2026-08-29 08:29:28 +0300

    refactor(types): remove unused translation model

--- src/v3/definition/app.ts
+++ src/v3/definition/app.ts
@@ -1,11 +1,5 @@
 export type StandardRecord = Record<string, unknown>;
 
-export type Translation = {
-	[key: string]: {
-		[key: string]: string;
-	};
-};
-
 export type Country = {
 	countryGuid: string;
 	countryCode: string;
