8a95e3ee0917a4c9b6925a606928a6a1ea24574c
rhahao <26148770+rhahao@users.noreply.github.com>
2026-08-29 08:22:59 +0300

    refactor(types): remove unused legacy models

--- src/platform/firebase/storage.types.ts
+++ src/platform/firebase/storage.types.ts
@@ -2,8 +2,3 @@ export type StorageBaseType = {
 	type: 'congregation' | 'user' | 'api';
 	path: string;
 };
-
-export type StorageCongregation = {
-	cong_discoverable?: { value: boolean; updatedAt: string };
-	last_backup?: string;
-};
--- src/v3/definition/ip.ts
+++
@@ -1,8 +0,0 @@
-export type IPAPIResponse = {
-	continent_code: string;
-	country_name: string;
-	country_code_iso3: string;
-	country_code: string;
-	city: string;
-	timezone: string | string[];
-};
